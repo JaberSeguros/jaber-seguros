2cd316724ce69f738ecba9701db1b1420bb79c54
SrDev-Henrique <halbuquerque2850@gmail.com>
2026-02-04 18:27:18 -0300

    chore: Add servicos and sobrenos pages

---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,7 @@
+export default function ServicosPage() {
+  return (
+    <div className="min-h-screen flex-center">
+      <p className="max-w-sm text-center">Aqui será a página de serviços.</p>
+    </div>
+  );
+}
---
+++ src/app/sobre-nos/page.tsx
@@ -0,0 +1,7 @@
+export default function SobreNosPage() {
+  return (
+    <div className="min-h-screen flex-center">
+      <p className="max-w-sm text-center">Aqui será a página sobre nós.</p>
+    </div>
+  );
+}
