c66695a4bc33d804c8b6d9c3d86a91e00db4a792
SrDev-Henrique <halbuquerque2850@gmail.com>
2026-02-12 15:40:34 -0300

    chore: Add how we work component

--- src/app/sobre-nos/components/how-we-work/how-we-work.tsx
+++ src/app/sobre-nos/components/how-we-work/how-we-work.tsx
@@ -1,14 +1,54 @@
+import { MessageCircleIcon, PinIcon, RocketIcon } from "lucide-react";
 import { AnimatedText } from "@/components/animated-text/animated-text";
+import { AnimatedImage } from "@/components/animted-image/animated-image";
+
+const items = [
+  {
+    id: 1,
+    content:
+      "Orientamos você desde a análise da necessidade até a implementação da solução.",
+    icon: <PinIcon className="shrink-0 text-foreground" size={32} />,
+  },
+  {
+    id: 2,
+    content:
+      "Você fala com especialistas que escutam, analisam e resolvem com agilidade.",
+    icon: <MessageCircleIcon className="shrink-0 text-foreground" size={32} />,
+  },
+  {
+    id: 3,
+    content:
+      "Processos organizados, suporte contínuo e atualização sempre que necessário.",
+    icon: <RocketIcon className="shrink-0 text-foreground" size={32} />,
+  },
+];
+
+const images = [
+  {
+    id: 1,
+    src: "/assets/variadas/pexels-mikhail-nilov-6530565.jpg",
+    alt: "Como a Jaber Seguros funciona - Transparência, planejamento, atendimento ágil e humano",
+    className:
+      "relative aspect-square max-h-[80vh] min-h-[400px] w-full overflow-hidden rounded-[0.33rem] md:aspect-2/3 md:max-h-[550px] md:min-h-none",
+  },
+  {
+    id: 2,
+    src: "/assets/variadas/pexels-boris-pavlikovsky-6793974.jpg",
+    alt: "Como a Jaber Seguros funciona - Transparência, planejamento, atendimento ágil e humano",
+    className:
+      "relative aspect-square size-full overflow-hidden rounded-[0.35rem] md:col-span-2 md:aspect-auto",
+  },
+];
 
 export function HowWeWork() {
   return (
     <section
       id="como-funciona-jaber"
-      className="w-full bg-border/40 py-15"
+      className="w-full bg-border/30 py-20 lg:py-35"
       aria-labelledby="how-we-work-heading"
       aria-label="Como a Jaber Seguros funciona - Transparência, planejamento, atendimento ágil e humano"
     >
-      <div className="w-full space-y-15">
+      <div className="mx-auto w-full max-w-[85rem] space-y-20 px-4">
         <header className="flex w-full flex-col gap-4">
           <p
             className="animate-fade-up uppercase [animation-delay:600ms]"
@@ -43,6 +83,29 @@ export function HowWeWork() {
             </div>
           </div>
         </header>
+        <div className="space-y-20">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            {images.map((image) => (
+              <AnimatedImage
+                key={image.id}
+                src={image.src}
+                alt={image.alt}
+                imageContainerClassName={image.className}
+              />
+            ))}
+          </div>
+          <div className="mx-auto grid w-full max-w-[85rem] grid-cols-1 gap-6 lg:grid-cols-3">
+            {items.map((item) => (
+              <div
+                key={item.id}
+                className="flex items-center gap-8 border-foreground/20 border-t pt-6 text-muted-foreground lg:justify-center"
+              >
+                {item.icon}
+                <p className="font-medium">{item.content}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
