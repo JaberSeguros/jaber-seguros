312ba68e41d6db561d73868c3894b002692cf68e
SrDev-Henrique <halbuquerque2850@gmail.com>
2026-02-13 12:06:30 -0300

    chore: remove unused import

--- src/app/(sections)/services/components/sustainable.tsx
+++ src/app/(sections)/services/components/sustainable.tsx
@@ -1,5 +1,4 @@
 import { ArrowRightIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { AnimatedImage } from "@/components/animted-image/animated-image";
 import { ServiceIntro } from "@/components/service-intro";
